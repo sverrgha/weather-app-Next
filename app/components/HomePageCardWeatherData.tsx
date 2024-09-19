6ac19723a4f508254c8ba5a15dc140bd9d7eff3a
Sverre Halvorsen <sverrgha@stud.ntnu.no>
2024-09-19 18:30:21 +0200

    Feat: Make new hook to choose icon based on weather-code

--- app/components/HomePageCardWeatherData.tsx
+++ app/components/HomePageCardWeatherData.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, use } from 'react'
 import useWeather from '../hooks/useWeather';
 import useWeatherIcon from '../hooks/useWeatherIcon';
 
@@ -9,7 +9,8 @@ interface HomePageCardWeatherDataProps {
 
 const HomePageCardWeatherData = (props: HomePageCardWeatherDataProps) => {
   const { weather, error, loading } = useWeather(props.city);
-  const weatherIconPath = useWeatherIcon(2);
+  const weatherIconPath = weather ? useWeatherIcon({ weatherCode: weather.weatherCode, isDay: weather.isDay }) : 
+    useWeatherIcon({ weatherCode: -1, isDay: -1 });
 
   return (
     <div>
@@ -21,7 +22,7 @@ const HomePageCardWeatherData = (props: HomePageCardWeatherDataProps) => {
         <div>
           <div className='flex flex-row justify-between p-2 items-center'>
             <p className='font-bold text-black text-3xl'>{`${weather.temperature}°C`}</p>
-            <img src={`images/icons/clearDay.svg`} alt='weather icon' className='w-12 h-12'/>
+            <img src={`${weatherIconPath}`} alt='weather icon' className='w-12 h-12'/>
           </div>
           <p className='font-bold text-black'>{`${weather.windSpeed}m/s ${weather.windDirection}°`}</p>
           <p className='font-bold text-black'>{`${weather.isDay ? "Day": "Night"}`}</p>
---
+++ app/hooks/useWeatherIcon.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+
+const getWeatherIcon = (weatherCode: number, isDay: number) => {
+  console.log(weatherCode, isDay);
+  if (weatherCode >= 0 && weatherCode <= 1) return isDay ? 'images/icons/clearDay.svg' : 'images/icons/clearNight.svg';
+  if (weatherCode == 2) return isDay ? 'images/icons/partlyOvercastDay.svg' : 'images/icons/partlyOvercastNight.svg';
+  if (weatherCode == 3) return 'images/icons/overcast.svg';
+  if (weatherCode >= 51 && weatherCode <= 67) return 'images/icons/rain.svg';
+  if (weatherCode >= 71 && weatherCode <= 77) return 'images/icons/snow.svg';
+  
+  return 'images/icons/loading.svg';
+}
+
+interface WeatherDataProps {
+  weatherCode: number;
+  isDay: number;
+}
+
+const useWeatherIcon = (props: WeatherDataProps) => {
+  const [icon, setIcon] = useState<string>('');
+
+  useEffect(() => {
+    setIcon(getWeatherIcon(props.weatherCode, props.isDay));
+  }, [props.weatherCode, props.isDay]);
+  return icon;
+}
+
+export default useWeatherIcon;
