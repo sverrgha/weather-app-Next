3a36b74b9a671f604aefcab6c29052c9902d0031
Sverre Halvorsen <sverrgha@stud.ntnu.no>
2024-09-19 17:28:26 +0200

    Feat: add weather icon inline with degrees on in HomePageCardWeatherData

--- app/components/HomePageCardWeatherData.tsx
+++ app/components/HomePageCardWeatherData.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState, useEffect } from 'react'
 import useWeather from '../hooks/useWeather';
+import useWeatherIcon from '../hooks/useWeatherIcon';
 
 interface HomePageCardWeatherDataProps {
   city: string;
@@ -8,6 +9,7 @@ interface HomePageCardWeatherDataProps {
 
 const HomePageCardWeatherData = (props: HomePageCardWeatherDataProps) => {
   const { weather, error, loading } = useWeather(props.city);
+  const weatherIconPath = useWeatherIcon(2);
 
   return (
     <div>
@@ -17,7 +19,10 @@ const HomePageCardWeatherData = (props: HomePageCardWeatherDataProps) => {
         <p>Error: {error}</p>
       ) : weather ? (
         <div>
-          <p className='font-bold text-black'>{`${weather.temperature}°C`}</p>
+          <div className='flex flex-row justify-between p-2 items-center'>
+            <p className='font-bold text-black text-3xl'>{`${weather.temperature}°C`}</p>
+            <img src={`images/icons/clearDay.svg`} alt='weather icon' className='w-12 h-12'/>
+          </div>
           <p className='font-bold text-black'>{`${weather.windSpeed}m/s ${weather.windDirection}°`}</p>
           <p className='font-bold text-black'>{`${weather.isDay ? "Day": "Night"}`}</p>
         </div>
