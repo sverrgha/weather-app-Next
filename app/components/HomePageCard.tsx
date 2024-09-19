d7b89dab061ffa84f9635901fda5f6b59b5955b8
Sverre Halvorsen <sverrgha@stud.ntnu.no>
2024-09-19 19:03:54 +0200

    Feat: Add seperating line under city name

--- app/components/HomePageCard.tsx
+++ app/components/HomePageCard.tsx
@@ -11,7 +11,8 @@ interface WeatherData {
 const HomePageCard = (props: HomePageCardProps) => {      
   return (
     <div className='flex-1 border border-neutral-950 p-2 rounded-lg bg-white'>
-      <h1 className='text-xl font-bold text-black'>{props.city}</h1>
+      <h1 className='text-3xl text-black font-serif'>{props.city}</h1>
+      <hr className='bg-black border-0' style={{height: 1 + "px"}}></hr>
       <HomePageCardWeatherData city={props.city} />
     
     </div>
