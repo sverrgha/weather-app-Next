22d6359060d9707e6203f96f2ed74b4d53923013
Sverre Halvorsen <sverrgha@stud.ntnu.no>
2024-09-23 17:56:17 +0200

    Fix: Resolve issue where snow/rain icon didnt appear on specific weather codes

--- app/hooks/useWeatherIcon.ts
+++ app/hooks/useWeatherIcon.ts
@@ -5,8 +5,8 @@ const getWeatherIcon = (weatherCode: number, isDay: number) => {
   if (weatherCode >= 0 && weatherCode <= 1) return isDay ? 'images/icons/clearDay.svg' : 'images/icons/clearNight.svg';
   if (weatherCode == 2) return isDay ? 'images/icons/partlyOvercastDay.svg' : 'images/icons/partlyOvercastNight.svg';
   if (weatherCode == 3) return 'images/icons/overcast.svg';
-  if (weatherCode >= 51 && weatherCode <= 67) return 'images/icons/rain.svg';
-  if (weatherCode >= 71 && weatherCode <= 77) return 'images/icons/snow.svg';
+  if ((weatherCode >= 51 && weatherCode <= 67) || (weatherCode >= 80 && weatherCode <= 83) ) return 'images/icons/rain.svg';
+  if ((weatherCode >= 71 && weatherCode <= 77) || (weatherCode >= 85 && weatherCode <= 86)) return 'images/icons/snow.svg';
   
   return 'images/icons/loading.svg';
 }
