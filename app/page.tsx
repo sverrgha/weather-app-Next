91169a0faf748890e488ca1a863403b0a15d4696
Sverre Halvorsen <sverrgha@stud.ntnu.no>
2024-09-24 07:31:29 +0200

    Feat: Add heading to front page

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,21 @@ import HomePageCard from "./components/HomePageCard";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col text-center h-screen items-center justify-center">
-        <div className="inline-block p-4" >
-          <input type="text" placeholder="Enter city name" className="input-bordered bg-white p-2 w-auto text-black rounded-lg" />
+      <div className="flex justify-center items-center min-h-screen p-4">
+        <div className="flex flex-col text-center min-h-screen items-center gap-y-12 w-full">
+          <h1 className="text-6xl text-black font-serif border-b-2 border-black">
+            Whats the weather?
+          </h1>
+          <input type="text" placeholder="Enter city name" className="bordered bg-white p-5 w-[50vw] text-black  rounded-full aspect-[10/1] text-xl" />
+          <div className="flex flex-row space-x-2 w-9/12">
+            <HomePageCard city="Oslo" />
+            <HomePageCard city="Trondheim" />
+            <HomePageCard city="Le Havre" />
+            <HomePageCard city="New York" />
+            <HomePageCard city="Madrid" />
+          </div>
         </div>
-        <div className="flex flex-row space-x-2 w-9/12">
-          <HomePageCard city="Oslo" />
-          <HomePageCard city="Trondheim" />
-          <HomePageCard city="Le Havre" />
-          <HomePageCard city="New York" />
-          <HomePageCard city="Madrid" />
-        </div>
-      </div>  
+      </div>
     </>
   );
 }
