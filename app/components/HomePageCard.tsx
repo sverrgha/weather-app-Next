e431bea809728ac11de6a9da4c16bddd03d3f7c4
Sverre Halvorsen <sverrgha@stud.ntnu.no>
2024-10-12 14:04:50 +0200

    Feat: Link to new page from the HomePageCard headers

--- app/components/HomePageCard.tsx
+++ app/components/HomePageCard.tsx
@@ -11,7 +11,7 @@ interface WeatherData {
 const HomePageCard = (props: HomePageCardProps) => {      
   return (
     <div className='flex-1 border border-neutral-950 p-2 rounded-lg bg-white'>
-      <h1 className='text-3xl text-black font-serif'>{props.city}</h1>
+      <Link className='text-3xl text-black font-serif' href={`/${props.city}`}>{props.city}</Link>
       <hr className='bg-black border-0' style={{height: 1 + "px"}}></hr>
       <HomePageCardWeatherData city={props.city} />
     
