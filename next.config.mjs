de2813a1969707e3bfef0b45cb05ed44160784a7
Sverre Halvorsen <sverrgha@stud.ntnu.no>
2024-10-24 16:51:17 +0200

    Fix: Resolve bug where application loaded infinitly

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000, // Check for changes every second
+      aggregateTimeout: 300, // Delay before rebuilding
+    }
+    return config
+  },
+}
 
 export default nextConfig;
